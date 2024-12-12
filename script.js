3ded6712a7c6f937af2cc92836d0ec83722238e8
Sam Annetts <samannetts8@gmail.com>
2024-12-12 15:50:31 +0000

    Minor CSS updates and restored js pseudo code

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 console.log("hello")
 
-// url for trivia site: https://opentdb.com/api.php?amount=10
+//HTML UI Section
 
-async function fetchData() {
-    try {
-      const response = await fetch('https://opentdb.com/api.php?amount=10');
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      // Handle your data
-      return data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      // Handle any errors
-    }
-  }
+//Ask user for their username and save response as variable
+//Create interactable "Ask me a question" button
+
+
+//API Request Section
+
+//Create a fetch request to grab one trivia question using API 
+//Convert the response to .json format 
+//Isolate question, correct answer, incorrect answers
+//Insert correct answer into incorrect answer array at random point
+//Update question element on HTML and print out potential answers
+//User inputs their answer
+//Evaluate against correct answer
+//Annouce if right or wrong
