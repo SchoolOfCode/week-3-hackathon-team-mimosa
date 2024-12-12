a8a308df0f84b81663c99d6ecd1fea140ba99ed4
Sam Annetts <samannetts8@gmail.com>
2024-12-12 14:14:19 +0000

    Psuedo code included in js file

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello")
+//HTML UI Section
+
+//Ask user for their username and save response as variable
+
+//Create interactable "Ask me a question" button
+
+
+
+
+
+
+
+//API Request Section
+
+//Create a fetch request to grab one trivia question using API 
+//Convert the response to .json format 
+//Isolate question, correct answer, incorrect answers
+//Insert correct answer into incorrect answer array at random point
+//Update question element on HTML and print out potential answers
+//User inputs their answer
+//Evaluate against correct answer
+//Annouce if right or wrong
