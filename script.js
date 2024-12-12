0e2205e05c0d3fddd69b0e3233f19a22801474e3
Monika <mciskowska@gmail.com>
2024-12-12 14:18:40 +0000

    m

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("hello")
+
+// url for trivia site: https://opentdb.com/api.php?amount=10
+
+async function fetchData() {
+    try {
+      const response = await fetch('https://opentdb.com/api.php?amount=10');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      // Handle your data
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle any errors
+    }
+  }
